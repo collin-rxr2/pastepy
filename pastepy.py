02c7675e7146f5ae2a1cb47abf5dda3bd8140fa5
rblxcollin <66873035+rblxcollin@users.noreply.github.com>
2022-11-18 17:49:53 +0100

    Fixed it
    My dumbass brain made an Error and then published on GitHub

--- pastepy.py
+++ pastepy.py
@@ -1,15 +1,24 @@
 import requests
 
 def request_paste(data):
-    return requests.post("https://pastebin.com/api/api_post.php", data=data)
+    return requests.post("https://pastebin.com/api/api_post.php", data=data).text
 
-def new(key, title, content, language):
-    data = {
-        "api_dev_key": key,
-        "api_paste_private": "0",
-        "api_paste_name": title,
-        "api_paste_code": content,
-        "api_option": "paste",
-        "api_paste_format": language
-    }
+def new(key, title, content, language="none"):
+    if language=="none":
+        data = {
+            "api_dev_key": key,
+            "api_paste_private": "0",
+            "api_paste_name": title,
+            "api_paste_code": content,
+            "api_option": "paste",
+        }
+    else:
+        data = {
+            "api_dev_key": key,
+            "api_paste_private": "0",
+            "api_paste_name": title,
+            "api_paste_code": content,
+            "api_option": "paste",
+            "api_paste_format": language
+        }
     return request_paste(data)
